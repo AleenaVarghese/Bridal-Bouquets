40d6761aeee19267c3b1c67556ddecf2f8a73e04
aleena-sayone <aleenav.sayone@gmail.com>
2019-06-27 15:36:44 +0530

    updating bouquets

--- bouquets/productAdd/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductaddConfig(AppConfig):
-    name = 'productAdd'
--- bouquets/productAdd/choices.py
+++
@@ -1,5 +0,0 @@
-BOUQUETS_CHOICES = ( (1, ("bride")),                    
-                (2,("groom")))
-
-FLOWER_CHOICES = ( (1, ("Rose")),               
-                (2,("Mixed Flower")))
